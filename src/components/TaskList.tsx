6ab7d644b0cbb866bfa51967cc1eb183a33efdbc
Luan Bresolla <luanpires34@gmail.com>
2021-03-15 20:37:12 -0300

    first commit

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle === '') {
+      return;
+    }
+
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.find(item => item.id === id);
+
+    if(task) {
+      task.isComplete = true;
+
+      const newTaskList = tasks.filter(task => task.id !== id);
+
+      setTasks([...newTaskList, task]);
+    }
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTaskList = tasks.filter(task => task.id !== id);
+
+    setTasks(newTaskList);
   }
 
   return (
